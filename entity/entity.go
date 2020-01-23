06b55ed3c7a25bbb73dda6bad7ea7065644b255f
178inaba <178inaba@users.noreply.github.com>
2020-01-23 21:52:10 +0900

    Add entity tag

--- entity/entity.go
+++ entity/entity.go
@@ -8,20 +8,20 @@ import (
 type User struct {
 	ID        uint
 	Name      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt sql.NullTime
+	CreatedAt time.Time    `db:"created_at"`
+	UpdatedAt time.Time    `db:"updated_at"`
+	DeletedAt sql.NullTime `db:"deleted_at"`
 }
 
 type Address struct {
 	ID          uint
-	UserID      uint
-	ZipCode     string
+	UserID      uint   `db:"user_id"`
+	ZipCode     string `db:"zip_code"`
 	Prefecture  string
-	Address1    string
-	Address2    string
-	PhoneNumber string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   sql.NullTime
+	Address1    string       `db:"address_1"`
+	Address2    string       `db:"address_2"`
+	PhoneNumber string       `db:"phone_number"`
+	CreatedAt   time.Time    `db:"created_at"`
+	UpdatedAt   time.Time    `db:"updated_at"`
+	DeletedAt   sql.NullTime `db:"deleted_at"`
 }
